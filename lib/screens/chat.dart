f0ab51eb5ff2b7a8705680c3ff57debbc3a2fbef
Denis Revin <denis.revin@gmail.com>
2022-02-20 18:42:19 +0400

    add geolocation

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 import 'package:surf_practice_chat_flutter/data/chat/repository/repository.dart';
 import 'package:surf_practice_chat_flutter/widgets/chat_message.dart';
 
+import '../data/chat/models/geolocation.dart';
 import '../data/chat/models/message.dart';
 
 /// Chat screen templete. This is your starting point.
@@ -84,6 +86,13 @@ class _ChatScreenState extends State<ChatScreen> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.share_location_outlined,
+                    color: Colors.purple,
+                  ),
+                  onPressed: () => _onSendLocation(),
+                ),
                 Expanded(
                   child: TextFormField(
                     decoration: const InputDecoration(labelText: 'Сообщение'),
@@ -153,5 +162,77 @@ class _ChatScreenState extends State<ChatScreen> {
         _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
       }
     });
+  }
+
+  void _onSendLocation() {
+    _showAlertDialog();
+  }
+
+  Future<void> _showAlertDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Отправить координаты?'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Отмена'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Отправить'),
+              onPressed: () async {
+                final locationData = await _getLocationData();
+                if (locationData != null) {
+                  widget.chatRepository.sendGeolocationMessage(
+                    nickname: _nickname,
+                    location: locationData,
+                  );
+                }
+
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<ChatGeolocationDto?> _getLocationData() async {
+    final location = Location();
+
+    var serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    var permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    final locationData = await location.getLocation();
+    return ChatGeolocationDto(
+      latitude: locationData.latitude ?? 0,
+      longitude: locationData.longitude ?? 0,
+    );
   }
 }
