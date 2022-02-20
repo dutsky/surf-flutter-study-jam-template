bfcee02ebb8e199589090a9f8ad774b6e6176091
Denis Revin <denis.revin@gmail.com>
2022-02-20 15:19:08 +0400

    highlight own messages

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../data/chat/models/message.dart';
+import 'package:surf_practice_chat_flutter/data/chat/chat.dart';
 
 class ChatMessageWidget extends StatelessWidget {
   final ChatMessageDto messageDto;
@@ -13,6 +12,9 @@ class ChatMessageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      tileColor: (messageDto.author is ChatUserLocalDto)
+          ? Colors.purple.withOpacity(0.1)
+          : null,
       leading: CircleAvatar(
         backgroundColor: Colors.deepPurple,
         child: Text(messageDto.author.name[0].toUpperCase()),
