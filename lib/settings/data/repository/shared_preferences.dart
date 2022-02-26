b56ce4c4808d5755da45e8fe7dd7bbd7aa82ec74
Denis Revin <denis.revin@gmail.com>
2022-02-26 06:09:49 +0400

    add nickname and theme methods to settings repo

--- lib/settings/data/repository/shared_preferences.dart
+++ lib/settings/data/repository/shared_preferences.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:surf_practice_chat_flutter/settings/data/models/app_settings.dart';
-import 'package:surf_practice_chat_flutter/settings/data/repository.dart';
+
+import '../models/app_settings.dart';
+import 'repository.dart';
 
 class SharedPreferencesSettingsRepository implements SettingsRepository {
   @override
   Future<AppSettings> loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    final nickname = prefs.getString('nickname');
+    final nickname = prefs.getString('nickname') ?? '';
+    final themeModeValue = prefs.getInt('themeMode') ?? ThemeMode.system.index;
+    final themeMode = ThemeMode.values[themeModeValue];
+
+    return AppSettings(
+      nickname: nickname,
+      themeMode: themeMode,
+    );
   }
 
   @override
@@ -14,6 +23,31 @@ class SharedPreferencesSettingsRepository implements SettingsRepository {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('nickname', settings.nickname);
     await prefs.setInt('themeMode', settings.themeMode.index);
-    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> loadNickname() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('nickname') ?? '';
+  }
+
+  @override
+  Future<void> saveNickname(String nickname) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('nickname', nickname);
+  }
+
+  @override
+  Future<ThemeMode> loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeModeValue = prefs.getInt('themeMode') ?? ThemeMode.system.index;
+    final themeMode = ThemeMode.values[themeModeValue];
+    return themeMode;
+  }
+
+  @override
+  Future<void> saveThemeMode(ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('themeMode', themeMode.index);
   }
 }
