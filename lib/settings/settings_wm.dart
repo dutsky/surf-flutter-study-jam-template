7bb7ce78596b66495cd329a269f9c010c5736525
Denis Revin <denis.revin@gmail.com>
2022-05-13 01:40:09 +0400

    refactor(settings): rename error handler

--- lib/settings/settings_wm.dart
+++ lib/settings/settings_wm.dart
@@ -1,9 +1,8 @@
 import 'package:elementary/elementary.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:surf_practice_chat_flutter/logger.dart';
 import 'bloc/settings_bloc.dart';
+import 'settings_error_hander.dart';
 import 'settings_model.dart';
 import 'settings_screen.dart';
 
@@ -14,7 +13,7 @@ abstract class ISettingsWidgetModel extends IWidgetModel {
 }
 
 SettingsWidgetModel settingsWidgetModelFactory(BuildContext context) {
-  final errorHandler = TestErrorHandler();
+  final errorHandler = SettingsErrorHandler();
   final settingsBloc = context.read<SettingsBloc>();
 
   return SettingsWidgetModel(SettingsModel(settingsBloc, errorHandler));
@@ -32,13 +31,4 @@ class SettingsWidgetModel extends WidgetModel<AppThemes, SettingsModel>
   @override
   void onThemeChange(ThemeMode? theme) =>
       _themeState.accept(model.theme = theme ?? ThemeMode.light);
-}
-
-class TestErrorHandler implements ErrorHandler {
-  @override
-  void handleError(Object error) {
-    if (kDebugMode) {
-      logger.e(error);
-    }
-  }
 }
