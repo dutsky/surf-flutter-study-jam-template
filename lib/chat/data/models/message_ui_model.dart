141a7d52da1cc35daa2d88edc92a9bb6e27a7eec
Denis Revin <denis.revin@gmail.com>
2022-05-16 13:40:19 +0400

    feat(chat): add message UI model

---
+++ lib/chat/data/models/message_ui_model.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'message_ui_model.freezed.dart';
+
+@freezed
+class MessageUIModel with _$MessageUIModel {
+  /// Data transfer object representing simple chat message.
+  const factory MessageUIModel.basic({
+    /// Message author.
+    required String author,
+
+    /// Chat message string.
+    required String message,
+
+    /// Creation date and time.
+    required String created,
+  }) = _MessageUIModel;
+
+  /// Data transfer object representing geolocation chat message.
+  const factory MessageUIModel.withLocation({
+    required String author,
+    required String message,
+    required String created,
+    required String latitude,
+    required String longitude,
+  }) = MessageUIModelWithLocation;
+}
